86c85da596e11984f86366bc5d8811c823d0a6ab
Brian Golden <bgold09@users.noreply.github.com>
2018-12-09 16:17:53 -0800

    Set up CI with Azure Pipelines (#3)
      * Compile code and run tests with Azure Pipelines

---
+++ iBucketList.UnitTests/SampleTests.cs
@@ -0,0 +1,15 @@
+using System;
+using iBucketList;
+using Xunit;
+
+namespace iBucketList.UnitTests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void AzurePipelinesValidationTest()
+        {
+            Assert.Equal(1, 1);
+        }
+   }
+}
