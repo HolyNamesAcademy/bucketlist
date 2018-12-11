e3e1c88431065e27aa7eacbb0f903972c8595bc3
MadronaM <31445221+MadronaM@users.noreply.github.com>
2018-12-11 07:43:55 -0800

    Goal Class Initialization (#4)

---
+++ iBucketList.Core/ArrayListofItems.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace iBucketList
+{
+    public class ArrayListofItems
+    {
+        public ArrayListofItems()
+        {
+            List<object> goals = new List<object>();
+
+        }
+    }
+}
