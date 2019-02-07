ec1e8850d2775b2e8370d1ec1eb447fc9a52cb37
Madrona Maling <madronamaling@Madronas-MacBook-Air.local>
2019-02-07 07:47:13 -0800

    Pop-Up and Button Created

---
+++ iBucketList/AddGoal.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace iBucketList
+{
+    public partial class AddGoal : ContentPage
+    {
+        public AddGoal()
+        {
+            InitializeComponent();
+        }
+
+        async void OnDismissButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
+
+//corresponds to the modal
+//describing the pop-up
+
