6d060d517eb49824ca6b0025e206cb0e1282e650
Madrona <MMaling19@holynames-sea.org>
2019-02-28 07:47:54 -0800

    Created 'Add Goal' button and stored information

--- iBucketList/AddGoal.xaml.cs
+++ iBucketList/AddGoal.xaml.cs
@@ -14,9 +14,24 @@ namespace iBucketList
         {
             await Navigation.PopModalAsync();
         }
+
+        async void CreateGoal(object sender, EventArgs args)
+        {
+            Goal goal = new Goal()
+            {
+                title = this.goalNameText.Text,
+                description = this.goalDescriptionText.Text,
+                //deadline = this.goaldeadlineText.Date
+            };
+            AppState.goals.AddGoal(goal);
+            await Navigation.PopModalAsync();
+        }
+
     }
 }
+
 
 //corresponds to the modal
 //describing the pop-up
 
+   //need to make date into date time from string
